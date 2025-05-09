897100ad5ecce73e47f2505ba40d20f107af9ea9
Tejas More <tejasmore660@gmail.com>
2025-05-09 17:36:16 +0530

    feat: add Mother component - Add component wrapper - Add state management - Add layout structure

---
+++ src/config/config.js
@@ -0,0 +1,10 @@
+
+export const MAX_IMAGES = parseInt(import.meta.env.VITE_MAX_IMAGES);
+
+export const MIN_IMAGES = parseInt(import.meta.env.VITE_MIN_IMAGES);
+
+export const MAX_IMG_SIZE = parseInt(import.meta.env.VITE_MAX_FILE_SIZE_MB);
+
+export const PAGE_PADDING_SIZE = parseInt(import.meta.env.VITE_PAGE_PADDING);
+
+export const CAPTION_HEIGHT_SIZE = parseInt(import.meta.env.VITE_CAPTION_HEIGHT_SPACE);
