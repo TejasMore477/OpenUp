5934c6ddb7ae7edff03551f24afa742966aaa835
Tejas More <tejasmore660@gmail.com>
2025-05-11 09:38:25 +0530

    sty(Loader):responsive

--- src/components/layout/Loader.jsx
+++ src/components/layout/Loader.jsx
@@ -8,7 +8,7 @@ function Loader() {
       initial={{ opacity: 1 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
-      transition={{ duration: 0.5 }}
+      transition={{ duration: 5 }}
     >
       <div
         role="status"
@@ -16,10 +16,10 @@ function Loader() {
         className="bg-darkTheme w-full min-h-screen overflow-hidden grid place-content-center px-4 sm:px-6 md:px-8"
       >
         <div className="text-center">
-          <h1 className="text-limicMist font-extrabold text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-Syncopate tracking-tighter leading-tight">
+          <h1 className="text-limicMist font-extrabold text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-Syncopate tracking-tighter leading-none">
             OpenUp
           </h1>
-          <div className="flex items-center justify-center mt-2 sm:mt-3 md:mt-4 text-limicMist font-light text-sm sm:text-base md:text-lg lg:text-xl font-Syncopate tracking-tighter">
+          <div className="flex items-center justify-center sm:mt-3 md:mt-4 text-limicMist font-light text-sm sm:text-base md:text-lg lg:text-xl font-Syncopate tracking-tighter">
             <p className="pr-2">Loading</p> <Counter /> <p>%</p>
           </div>
         </div>
