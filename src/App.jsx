8bf662220b7bed4991de98cc1ad67c2e97db78e1
Tejas More <tejasmore660@gmail.com>
2025-05-11 14:09:59 +0530

    all responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import React, { useEffect, useState, useCallback } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/layout/Header';
-import Footer from './components/layout/Footer';
-import Landing from './components/layout/Landing';
-import Loader from './components/layout/Loader';
-import ImgToPdf from './components/logic/ImgToPdf';
-import ImageThumbnail from './components/logic/ImageThumbnail';
-import Counter from './components/common/Counter';
-import Thumbnails from './components/common/Thumbnails';
-import About from './components/layout/About';
-import Features from './components/layout/Features';
-import PrivacyPolicy from './components/layout/PrivacyPolicy';
-import TermsOfService from './components/layout/TermsOfService';
+import React, { useEffect, useState, useCallback } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
+import Landing from "./components/layout/Landing";
+import Loader from "./components/layout/Loader";
+import ImgToPdf from "./components/logic/ImgToPdf";
+import Thumbnails from "./components/common/Thumbnails";
+import About from "./components/layout/About";
+import Features from "./components/layout/Features";
+import PrivacyPolicy from "./components/layout/PrivacyPolicy";
+import TermsOfService from "./components/layout/TermsOfService";
 
 function Home() {
   const [images, setImages] = useState([]);
@@ -21,50 +19,52 @@ function Home() {
   }, []);
 
   const handleRotate = useCallback((index) => {
-    setImages(prevImages => {
+    setImages((prevImages) => {
       const newImages = [...prevImages];
       newImages[index] = {
         ...newImages[index],
-        rotation: (newImages[index].rotation || 0) + 90
+        rotation: (newImages[index].rotation || 0) + 90,
       };
       return newImages;
     });
   }, []);
 
   const handleRemove = useCallback((index) => {
-    setImages(prevImages => prevImages.filter((_, i) => i !== index));
+    setImages((prevImages) => prevImages.filter((_, i) => i !== index));
   }, []);
 
   const handleCaptionChange = useCallback((index, caption) => {
-    setImages(prevImages => {
+    setImages((prevImages) => {
       const newImages = [...prevImages];
       newImages[index] = {
         ...newImages[index],
-        caption
+        caption,
       };
       return newImages;
     });
   }, []);
 
   return (
-    <>
+    <div className="flex flex-col-reverse sm:flex-col items-center justify-center">
       <Landing />
-      {/* Converter Section */}
-      <div className="backdrop-blur-sm bg-white/5 rounded-xl p-8 border border-white/20">
-        <ImgToPdf onImagesChange={setImages} images={images} />
-      </div>
-
-      {images.length > 0 && (
-        <div className="mt-8">
-          <Thumbnails
-            images={images}
-            onRotate={handleRotate}
-            onRemove={handleRemove}
-            onCaptionChange={handleCaptionChange}
-          />
+      <div>
+        {/* Converter Section */}
+        <div className="sm:backdrop-blur-sm sm:bg-white/5 rounded-xl sm:p-8 sm:border sm:border-white/20">
+          <ImgToPdf onImagesChange={setImages} images={images} />
         </div>
-      )}
-    </>
+
+        {images.length > 0 && (
+          <div className="mt-8">
+            <Thumbnails
+              images={images}
+              onRotate={handleRotate}
+              onRemove={handleRemove}
+              onCaptionChange={handleCaptionChange}
+            />
+          </div>
+        )}
+      </div>
+    </div>
   );
 }
 
@@ -85,9 +85,9 @@ function App() {
 
   return (
     <Router>
-      <div className='bg-darkTheme min-h-screen'>
+      <div className="bg-darkTheme min-h-screen">
         <Header />
-        <main className='container mx-auto px-4 py-8'>
+        <main className="container mx-auto px-4 py-8">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
