92985b7e382f7c14abea2d67cab4d24138592b59
Tejas More <tejasmore660@gmail.com>
2025-05-11 14:08:29 +0530

    styles(loader):responsive

--- src/components/layout/Loader.jsx
+++ src/components/layout/Loader.jsx
@@ -8,7 +8,7 @@ function Loader() {
       initial={{ opacity: 1 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
-      transition={{ duration: 5 }}
+      transition={{ duration: 0.5 }}
     >
       <div
         role="status"
